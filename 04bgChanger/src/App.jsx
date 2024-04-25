42d9215e7991f66bf8b637d405cf681a1b4bf66c
irohanrajput <itusharsingh27@gmail.com>
2024-04-26 04:59:09 +0530

    without using components

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,20 +1,46 @@
 import { useState } from "react"
-import Changer from './components/changers'
 
 
 function App() {
-  const [color, setColor] = useState("white")
+  const [color, setColor] = useState("grey")
 
   return (
-    <div className="w-full h-screen duration-200"
+    <div className=" h-screen duration-200 flex items-center justify-center text-white font-bold text-xl p-7"
     style={{backgroundColor: color}}
-    >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 rounded ">
+    >Click on the buttons below to change the background color.
+    <br />
+    this is {color} background
+      {/* <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 rounded ">
 
-        <Changer  color="red"/>
+        <Changer color="red"/>
         <Changer color="green"/>
         <Changer color="orange"/>
         <Changer color="black"/>
+      </div> */}
+      <div
+      className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 rounded ">
+      
+      <button onClick={() => setColor("red")} className="px-4, rounded-full"
+      style={{backgroundColor:"red"}}>
+        red
+      </button>
+
+      <button onClick={() => setColor("green")} className="px-4, rounded-full"
+      style={{backgroundColor:"green"}}>
+        green
+      </button>
+
+      <button onClick={() => setColor("blue")} className="px-4, rounded-full"
+      style={{backgroundColor:"blue"}}>
+        blue
+      </button>
+
+      <button onClick={() => setColor("black")} className="px-4, rounded-full"
+      style={{backgroundColor:"black"}}>
+        blue
+      </button>
+
+
       </div>
     </div>
   )
